75f2337b7a3d339196b9848fa3d0e3417a0d74b3
Ahammed <66068025+asaad227@users.noreply.github.com>
2023-05-25 22:50:16 +0100

    update css display

--- src/App/App.js
+++ src/App/App.js
@@ -5,6 +5,7 @@ import libary from '../lib';
 function App() {
 const tax = libary;
   const [personalIncome, setPersonalIncome] = useState(0);
+  const [annualIncome, setAnnualIncome] = useState(0);
   const [ni, setNi] = useState(0);
   const [incomeTax, setIncomeTax] = useState(0);
   const [netIncome, setNetIncome] = useState(0);
@@ -31,6 +32,8 @@ const tax = libary;
     setNi(nationalInsurance);
     setIncomeTax(incomeTax);
     setData(totalTax);
+    setAnnualIncome(personalIncome);
+    setPersonalIncome(0);
   event.preventDefault();
   }
 
@@ -44,7 +47,7 @@ const tax = libary;
       <form onSubmit={handleSubmit}>
       <label>
           Tax Year:
-          <select value={year} onChange={handleChange}>
+                    <select value={year} onChange={handleChange}>
             <option value="1">2022/23</option>
             <option value="2">2023/24</option>
             <option value="3">2024/25</option>
@@ -59,12 +62,17 @@ const tax = libary;
         <input type="submit" value="Submit" />
       </form>
       <div className='flex-container result'>
-      <p>National Insurance: {ni.toFixed(2)}</p>
-      <p>Income Tax: {incomeTax.toFixed(2)}</p>
-      <p>Total Tax: {data.toFixed(2)}</p>
-      <p>Take Home payment: {netIncome.toFixed(2)}</p>
-      <p>Take Home payment per month: {monthlyIncome.toFixed(2)}</p>
-      <p>Take Home payment per week: {weeklyIncome.toFixed(2)}</p>
+      <h2>Results</h2>
+      <div className='divider'></div>
+      <p>Annual Income: <span className='income'>£{annualIncome}</span></p>
+      <p>National Insurance: <span className='tax'>£{ni.toFixed(2)}</span></p>
+      <p>Income Tax: <span className='tax'>£{incomeTax.toFixed(2)}</span> </p>
+      <p>Total Tax including NI: <span className='tax'>£{data.toFixed(2)}</span></p>
+      <div className='divider'></div>
+      <p>Net Income: <span className='income'>£{netIncome.toFixed(2)}</span></p>
+      <p>Net Income per month: <span className='income'>£{monthlyIncome.toFixed(2)}</span></p>
+      <p>Net Income per week: <span className='income'>£{weeklyIncome.toFixed(2)}</span></p>
+      <div className='no-divider'></div>
       </div>
      
 
