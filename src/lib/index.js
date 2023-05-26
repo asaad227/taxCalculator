6a00147140dd5378ab285337766176f7ce0be4a7
Ahammed <66068025+asaad227@users.noreply.github.com>
2023-05-26 01:08:39 +0100

    fixed income tax allowances error

--- src/lib/index.js
+++ src/lib/index.js
@@ -6,11 +6,11 @@ export function taxCalculator(year, income){
      }
          let totalTax = 0;
          if (year === 1) {
-             if (income > 12571 && income < 50270) {
+             if (income > 12570 && income <= 50270) {
                  totalTax = (income - 12570) * rates.basicRate;
              } else if (income > 50270 && income < 125140) {
                  totalTax = (income - 50270) * rates.higherRate + 37700 * rates.basicRate;
-             } else if (income > 125140) {
+             } else if (income >= 125140) {
                  totalTax = (income - 125140) * rates.additionalRate + 74870 * rates.higherRate + 37700 * rates.basicRate;
              }
          }
@@ -22,17 +22,14 @@ export function nationalInsurance(year, income){
      const rates = {
          basicRate: 0.12,
          higherRate: 0.02,
-         additionalRate: 0.02
      }
          let totalNi = 0;
          if (year === 1) {
-             if (income > 12570 && income <= 50250) {
-                 totalNi = (income - 12570) * rates.basicRate;
-             } else if (income > 50250 && income < 150000) {
-                 totalNi = (income - 50250) * rates.higherRate + 40500 * rates.basicRate;
-             } else if (income > 150000) {
-                 totalNi = (income - 150000) * rates.additionalRate + 100000 * rates.higherRate + 40500 * rates.basicRate;
-             }
+             if (income > 12576 && income <= 50268) {
+                 totalNi = (income - 12576) * rates.basicRate;
+             } else if (income > 50268) {
+                 totalNi = (income - 50250) * rates.higherRate + 37692 * rates.basicRate;
+             } 
          }
          return totalNi;
  
