611471c10b076e569fb81335a7448ff7925e3432
Ahammed <66068025+asaad227@users.noreply.github.com>
2023-05-31 21:25:20 +0100

    error page handeler added to the project

--- src/App/App.js
+++ src/App/App.js
@@ -5,15 +5,17 @@ import England from '../TotalTax';
 import Scotland from '../Scottland';
 import Wales from '../Wales';
 import Home from '../Home';
+import ErrorPage from '../ErrorPage/errorPage';
 
 export default function App() {
   return (
     <div>
       <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="*" element={<ErrorPage/>}/>
           <Route path="/england" element={<England/>}/>
           <Route path="/scotland" element={<Scotland/>}/>
           <Route path="/wales" element={<Wales/>}/>
-          <Route path="/" element={<Home/>}/>
       </Routes>
       
     </div>
