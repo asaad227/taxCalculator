c03fd1a7a8931485f18649ed10047ccc6c7dfcb1
Ahammed <66068025+asaad227@users.noreply.github.com>
2023-06-02 20:55:29 +0100

    dark mode toggle added to the project

--- src/Nav-bar/index.js
+++ src/Nav-bar/index.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React,{useState} from 'react';
 import "./index.css";
 import { Link } from 'react-router-dom';
-
+/*
+  --main-bg-color: #ffffff;
+  --main-text-color: #990099;
+  --main-border-color: #668086;
+  --main-shadow-color: rgba(0,0,0,0.2);
+*/
 export default function NavBar() {
+  const [toggle, setToggle] = useState(false);
+ 
+  function handleClick(color1,color2,color3,color4){
+    setToggle(!toggle);
+    /*
+      --main-bg-color: #ffffff;
+  --main-text-color: #990099;
+  --main-border-color: #668086;
+  --main-shadow-color: rgba(0,0,0,0.2); */
+  if(toggle){
+    document.documentElement.style.setProperty('--main-bg-color', color1);
+    document.documentElement.style.setProperty('--main-text-color', color2);
+    document.documentElement.style.setProperty('--main-border-color', color3);
+    document.documentElement.style.setProperty('--main-shadow-color', color4);
+  }else{
+    color1 = '#ffffff';
+    color2 = '#990099';
+    color3 = '#668086';
+    color4 = 'rgba(0,0,0,0.2)';
+    document.documentElement.style.setProperty('--main-bg-color', color1);
+    document.documentElement.style.setProperty('--main-text-color', color2);
+    document.documentElement.style.setProperty('--main-border-color', color3);
+    document.documentElement.style.setProperty('--main-shadow-color', color4);
+  }
+  }
+
   return (
  
         <nav>
+         <i className={toggle? 'fas fa-toggle-off':'fas fa-toggle-on'} onClick={()=>handleClick('black','white','yellow','#f6f6f6')}></i>
             <h1>Tax Calculator UK</h1>
-            <span className="material-symbols-outlined">
+           
+            {/* <span className="material-symbols-outlined">
 calculate
-</span>
+</span> */}
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/england">England</Link></li>
